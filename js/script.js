ac0ee62ea02ca3ec520c1e336e81b0cfc511e44c
@BagliettoAron <aronbaglietto88@gmail.com>
2022-04-06 15:20:48 +0200

    Add: scaffolding, .ms_password title in html, password generator script, .ms_password css properties

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// INPUT
+
+// data 1: user name
+const userName = prompt ('What\'s your name?')
+// test
+console.log(userName)
+// data 2: user surname
+const userSurname = prompt ('Whats\'s your surname')
+// test
+console.log (userSurname)
+// data 3: favoritecolour 
+const favouriteColour = prompt ('Whats\'s your favourite colour?')
+// test
+console.log (favouriteColour)
+
+// ELABORATION
+const myPassword = `${userName}${userSurname}${favouriteColour}`
+// test
+console.log (myPassword)
+
+// OUTPUT
+document.getElementById('my-Psw').innerHTML = myPassword;
